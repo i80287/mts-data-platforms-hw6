7db6d060c6ede2b5ef491e6188c769be4858776a
i80287 <addmlbx@gmail.com>
2025-03-29 00:03:16 +0300

    prettify scripts

--- scripts/detail/create_xtable.bash
+++ scripts/detail/create_xtable.bash
@@ -2,6 +2,8 @@
 
 gpfdist -p 8807 -d /home/user/team-5-data &
 
+psql -d idp -c "DROP EXTERNAL TABLE IF EXISTS team_5_external_table_for_spark_csv;"
+
 psql -d idp -c "CREATE EXTERNAL TABLE team_5_external_table_for_spark_csv (
     brand text,
     item_id bigint,
@@ -17,6 +19,6 @@ psql -d idp -c "CREATE EXTERNAL TABLE team_5_external_table_for_spark_csv (
 LOCATION ('gpfdist://localhost:8807/for_spark.csv')
 FORMAT 'CSV' (HEADER);"
 
-psql -d idp -c "SELECT * FROM TABLE team_5_external_table_for_spark_csv;"
+psql -d idp -c "SELECT * FROM team_5_external_table_for_spark_csv LIMIT 20;"
 
-pgrep --full "gpfdist -p 8807" | xarg kill
+pgrep --full "gpfdist -p 8807" | xargs kill
